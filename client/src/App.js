f121c31eb290f0a510f32453bd1ea5e4d97f0778
Victor Dar <victordar@mail.ru>
2025-09-05 22:41:32 +0300

    16.10 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 import TableContact from "./layout/tableContact/TableContact";
+import { useState } from "react";
+
 
 const App = () => {
+  const [contacts, setContacts] = useState(
+    [
+      { id: 1, name: 'Имя Фамилия1', email: "fake1" },
+      { id: 2, name: 'Имя Фамилия2', email: "fake2" },
+      { id: 3, name: 'Имя Фамилия3', email: "fake3" }
+    ]
+);
+
+  const addContact = () => {
+    const item = {
+      id: contacts.sort((x, y) => x.id - y.id)[contacts.length - 1].id + 1,
+      name: 'Имя Фамилия4',
+      email: "fake4"
+    };
+    setContacts([...contacts, item]); // создаем новый массив на основе старого + новый элемент
+    console.log(contacts);
+  }
+
+
   return (
     <div className="container mt-5">
       <div className="card">
@@ -9,7 +30,14 @@ const App = () => {
         </div>
 
         <div className="card-body">
-          <TableContact></TableContact>
+          <TableContact contacts={contacts}></TableContact>
+          <div>
+            <button
+              className="btn btn-primary"
+              onClick={() => { addContact() }}>
+              Добавить контакт
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/layout/tableContact/TableContact.jsx
+++ client/src/layout/tableContact/TableContact.jsx
@@ -1,21 +1,29 @@
 import RowTableContact from "./components/RowTableContact";
 
-const TableContact = () => {
-    return (
-                  <table className="table table-hover table-bordered">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Имя контакта</th>
-                <th>E-mail</th>
-              </tr>
-            </thead>
-            <tbody>
-                <RowTableContact></RowTableContact>
-                <RowTableContact></RowTableContact>
-                <RowTableContact></RowTableContact>
-            </tbody>
-          </table>
-    )
+
+const TableContact = (props) => {
+  return (
+    <table className="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Имя контакта</th>
+          <th>E-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.contacts.map(
+            contact =>
+              <RowTableContact
+                id={contact.id}
+                name={contact.name}
+                email={contact.email}>
+              </RowTableContact>
+          )
+        }
+      </tbody>
+    </table>
+  )
 }
 export default TableContact;
